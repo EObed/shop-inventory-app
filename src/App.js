16abdbbcfd16ce5e124baf9013390608c8456136
EObed <obedeffum10@gmail.com>
2022-12-06 03:03:29 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="main-page">
       <div className="title-bar">
-        <p>lorem</p>
+        <div><img src='/logo1.png' alt='MyShop Logo'></img></div>
+        <div className='header'>
+        <h1>Welcome to the MyShop App</h1>
       </div>
+      <div className='buttons'>
+        <div><button>Log In</button></div>
+        <div><button>Sign Up</button></div>
+      </div>
+      </div>
+      <div className='content-box'>
+        <div className='content-header'><p>Shopkeeping Made Easier</p></div>
+        <div className='text-boxes'></div>
+      </div>
+    
     </div>
   );
 }
