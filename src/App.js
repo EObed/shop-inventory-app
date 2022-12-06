b0b23766768a66178bb63f5dc6853cbc14ff5c2f
EObed <obedeffum10@gmail.com>
2022-12-06 14:08:53 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+
+
 
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 3
+  };
+
   return (
     <div className="main-page">
       <div className="title-bar">
@@ -17,8 +30,17 @@ function App() {
       </div>
       <div className='content-box'>
         <div className='content-header'><p>Shopkeeping Made Easier</p></div>
-        <div className='text-boxes'></div>
+        <Slider {...settings}>
+        <div className='text-boxes'>
+         <div className='text-box1'></div>
+         <div className='text-box2'></div>
+         <div className='text-box3'></div>
+        </div>
+        </Slider>
+        
       </div>
+        
+       
     
     </div>
   );
---
+++ src/Carousel.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import './App.css';
+
+export default function index() {
+     
+}
+    
