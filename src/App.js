e71cf022e9b2ba25d57f0399c06f6d7c0f2f3440
EObed <obedeffum10@gmail.com>
2022-12-07 11:40:17 +0000

    update

--- src/App.js
+++ src/App.js
@@ -26,8 +26,28 @@ function App() {
                 <div className="content-header">
                     <p>Shopkeeping Made Easier</p>
                 </div>
-                <div className="text-boxes">
-                  <Slider/>
+                <div className="main-content">
+                    <div className="description">
+                        <p className="description-heading">
+                            <h3>What is the MyShop App?</h3>
+                        </p>
+                        <p className="description-body">
+                            The MyShop App is an on-line shopkeeping app. It has an inventory, to-do list among other tools
+                            to make your work as a shop-keeper easier.
+                        </p>
+                    </div>
+                    <div className="text-boxes">
+                    <Slider/>
+                    </div>
+                </div>
+            </div>
+          
+            <div className="footer">
+                <div className="logo-down">
+                    <img src="/logo1.png" alt="MyShop Logo" className="logo-bottom"></img>
+                </div>
+                <div className="contact-info">
+
                 </div>
             </div>
         </div>
